c2fa0061098985d05ba78e662c82c51f08f063a5
manolo_tambien <magpags@gmail.com>
2023-04-16 17:19:32 -0600

    Agregar archivos de proyecto.

---
+++ ApiAviones/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiAviones.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAviones.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public  ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+        // Conforme se van creando tablas se agregan aquí las clases de los modelos.
+        public DbSet<Avion> Avion { get; set; }
+    }
+}
