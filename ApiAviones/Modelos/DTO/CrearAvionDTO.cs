29532a07090a9a55d21c17f0613d63a646268b8b
manolo_tambien <magpags@gmail.com>
2023-04-16 19:28:37 -0600

    Creacion de DTO para Avion
    Se crean los dos DTO para Avion, en uno se quita el ID del DTO para que automaticamente la base de datos le agregue el ID, es por eso que se crea ese DTO sin el ID.

---
+++ ApiAviones/Modelos/DTO/CrearAvionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiAviones.Modelos.DTO
+{
+    public class CrearAvionDTO
+    {
+        /// <summary>
+        ///  Esta validación es importante para el nombre porque sino se crea vacia el nombre de categoria.
+        /// </summary>
+        [Required(ErrorMessage ="El nombre es obligatorio.")]
+        [MaxLength(60,ErrorMessage ="El número máximo de caracteres es de 60")]
+        public string Nombre { get; set; }
+    }
+}
