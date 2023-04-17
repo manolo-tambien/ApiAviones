870b01e26025236611db48f1ba3b63b8d4578efd
manolo_tambien <magpags@gmail.com>
2023-04-16 19:41:41 -0600

    Creación de AvionMapper
    Se crea la clase AvionMapper para indicar los diferentes tipos de automapper que vamos a utilizar para la creación de un avion (CrearAvionDTO) y para consultar el avion a la base de datos (AvionDTO).

---
+++ ApiAviones/AvionesMapper/AvionesMapper.cs
@@ -0,0 +1,16 @@
+﻿
+using ApiAviones.Modelos;
+using ApiAviones.Modelos.DTO;
+using AutoMapper;
+
+namespace ApiAviones.AvionesMapper
+{
+    public class AvionesMapper : Profile
+    {
+        public AvionesMapper()
+        {
+            CreateMap<Avion, AvionDTO>().ReverseMap();
+            CreateMap<Avion, CrearAvionDTO>().ReverseMap();
+        }
+    }
+}
