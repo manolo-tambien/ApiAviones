f69a05189d5c4f7022a71b0c799b3e25d44ca63e
manolo_tambien <magpags@gmail.com>
2023-05-10 16:32:02 -0600

    Create AeropuertoDTO.cs
    Se crea el Dto de Aeropuerto junto con sus decoradores

---
+++ ApiAviones/Modelos/DTO/AeropuertoDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiAviones.Modelos.DTO
+{
+	public class AeropuertoDTO
+	{
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del aeropuerto es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La cantidad de aforo es obligatoria.")]
+        public string Aforo { get; set; }
+
+        public DateTime FechaDeCreacion { get; set; }
+
+        
+        public int avionId { get; set; }
+    }
+}
+
